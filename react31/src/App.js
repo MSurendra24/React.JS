5df237e9a0a2c1f2b1acd5ef0b961178964937c0
MSurendra24 <mettelasurendra2004@gmail.com>
2025-08-07 11:41:27 +0530

    one

--- react31/src/App.js
+++ react31/src/App.js
@@ -1,12 +1,16 @@
 import Header from './components/Header'
 import Footer from './components/Footer';
-import Movies from './Movies/Movies'
+import movies from './movies/movies';
 function App(){
     
     return <div>
                 <h2>App Component</h2>
                 <hr/>
-                <Movies/>
+                <Header/>
+                <Footer/>
+                <movies/>
+                <hr/>
+            
             </div>
 }
 
---
+++ react31/src/movies/movies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class movies extends React.Component{
+
+    render(){
+        return <div>
+                    <h2>Movie Component</h2>
+                </div>
+    }
+}
+export default movies
