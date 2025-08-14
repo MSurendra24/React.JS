430810093942facc36809d3363421d123ddd31c2
MSurendra24 <mettelasurendra2004@gmail.com>
2025-08-14 10:20:33 +0530

    start

--- react31/src/App.js
+++ react31/src/App.js
@@ -2,6 +2,8 @@ import Product from './Message/Product';
 import Product1 from './Message/Product1';
 import CompA from './Message/CompA';
 import CompB from './Message/CompB';
+import Message1 from './Message/Message1';
+import Messages from './Message/Messages';
 function App() {
 return <div>
     <h1>Welcome to Product Management</h1>
@@ -9,7 +11,13 @@ return <div>
     <Product1 />
     <CompA />
     <CompB />
-    <h2>End of Components</h2>
+    <hr />
+    <h2>Components</h2>
+    <Message1 />
+    <h2>End of Product Management</h2>
+    <hr />
+    <h2>Values</h2>
+    <Messages />
     <hr />
     </div>
 
